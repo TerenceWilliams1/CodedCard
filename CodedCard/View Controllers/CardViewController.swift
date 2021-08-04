61efac088dce208eff67857bc7ced5cce53c23ee
Terence Williams <terencewilliams15@gmail.com>
2021-08-04 16:06:08 -0500

    QR links + logic

--- CodedCard/View Controllers/CardViewController.swift
+++ CodedCard/View Controllers/CardViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DTOverlayController
 
 class CardViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -132,6 +133,106 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
         cell.titleLabel.text = item.rawValue.capitalized
         cell.iconImageView.image = UIImage(named: item.rawValue)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = sections[indexPath.row]
+        
+        let qrCodeViewController = self.storyboard?.instantiateViewController(withIdentifier: "QRCodeViewController") as! QRCodeViewController
+        qrCodeViewController.item = item
+        qrCodeViewController.url = code(_forSection: item)
+
+        let overlayController = DTOverlayController(viewController: qrCodeViewController)
+        overlayController.overlayHeight = .dynamic(0.6)
+        overlayController.isPanGestureEnabled = true
+        present(overlayController, animated: true, completion: nil)
+    }
+    
+    func code(_forSection section: CardSection) -> String {
+        switch section {
+        case .address:
+            return CardHelper.valueForKey(key: CardSection.address.rawValue)
+        case .phone:
+            return CardHelper.valueForKey(key: CardSection.phone.rawValue)
+        case .email:
+            return CardHelper.valueForKey(key: CardSection.email.rawValue)
+        case .website:
+            return CardHelper.valueForKey(key: CardSection.website.rawValue)
+        case .linkedin:
+            let link = CardHelper.valueForKey(key: CardSection.linkedin.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.linkedin.com/in/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.linkedin.rawValue)
+        case .facebook:
+            let link = CardHelper.valueForKey(key: CardSection.facebook.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.facebook.com/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.facebook.rawValue)
+        case .instagram:
+            let link = CardHelper.valueForKey(key: CardSection.instagram.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.instagram.com/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.instagram.rawValue)
+        case .twitter:
+            let link = CardHelper.valueForKey(key: CardSection.twitter.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.twitter.com/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.twitter.rawValue)
+        case .snapchat:
+            let link = CardHelper.valueForKey(key: CardSection.snapchat.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.snapchat.com/add/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.snapchat.rawValue)
+        case .tiktok:
+            let link = CardHelper.valueForKey(key: CardSection.tiktok.rawValue)
+            if ((!isValid(link: link)) || (!link.contains("@"))) {
+                return "https://www.tiktok.com/@\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.tiktok.rawValue)
+        case .whatsapp:
+            let link = CardHelper.valueForKey(key: CardSection.whatsapp.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.whatsapp.com/send?phone=\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.whatsapp.rawValue)
+        case .youtube:
+            return CardHelper.valueForKey(key: CardSection.youtube.rawValue)
+        case .cashapp:
+            let link = CardHelper.valueForKey(key: CardSection.cashapp.rawValue)
+            if ((!isValid(link: link)) || (!link.contains("$"))) {
+                return "https://www.cash.app/$\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.cashapp.rawValue)
+        case .twitch:
+            let link = CardHelper.valueForKey(key: CardSection.twitch.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.twitch.tv/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.twitch.rawValue)
+        case .github:
+            let link = CardHelper.valueForKey(key: CardSection.github.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.github.com/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.github.rawValue)
+        case .soundcloud:
+            let link = CardHelper.valueForKey(key: CardSection.soundcloud.rawValue)
+            if (!isValid(link: link)) {
+                return "https://www.soundcloud.com/\(link)"
+            }
+            return CardHelper.valueForKey(key: CardSection.soundcloud.rawValue)
+        default:
+            return ""
+        }
+    }
+    
+    func isValid(link: String) -> Bool {
+        return link.contains(".com")
     }
 
 }
---
+++ CodedCard/View Controllers/QRCodeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  QRCodeViewController.swift
+//  CodedCard
+//
+//  Created by Terence Williams on 8/4/21.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+    
+    @IBOutlet weak var QRImageView: UIImageView!
+    
+    var item: CardSection = .avatar
+    var url: String? = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        QRImageView.layer.cornerRadius = 24
+        QRImageView.clipsToBounds = true
+        
+        if let QRImage = generateQRCode(from: url ?? "") {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.QRImageView.image = QRImage
+            }
+        }
+    }
+    
+    //MARK: - Helpers
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
+
+}
